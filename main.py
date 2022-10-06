46342faee4e87ec57f20de87f04e1c07a04b799c
icelik91 <106541769+icelik91@users.noreply.github.com>
2022-10-06 17:16:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="New Site,how is it", page_icon=":zap:", layout="wide")
+st.subheader("Hello there, I'm İbrahim")
+st.title("Experimenting with web stuff")
+st.write("Tis should make CV/resume sending transform into ""showing it""")
